ea52d58eeef7720d0eb4deefc42083340687d1fe
Josh Moore <josh@openmicroscopy.org>
2024-10-02 11:49:25 +0200

    Second parse.py implementation

--- stats/parse.py
+++ stats/parse.py
@@ -1,35 +1,37 @@
 #!/usr/bin/env python
-from __future__ import annotations
-
-import shutil
-import tempfile
-
 import fastparquet
 import pandas as pd
 
-cols = {"source", "origin", "url", "written_human_readable", "written"}
+cols = [ 'source', 'origin', 'url', 'written_human_readable', 'written']
 valid = []
-with tempfile.TemporaryDirectory() as tmpdirname:
-    print("created temporary directory", tmpdirname)
 
-    df = pd.read_csv("ngff_samples.csv")
-    urls = []
-    for index, row in df.iterrows():
-        src = row["source"]
-        url = row["url"]
-        print(url)
-        urls.append(url)
+df = pd.read_csv('ngff_samples.csv')
+urls = []
+for index, row in df.iterrows():
 
-        df2 = pd.read_csv(f"{url}")
-        if not set(df2.columns).issubset(cols):
-            print(f"invalid csv: {url}")
-        else:
-            filename = f"{tmpdirname}/index.pq"
-            df2.to_parquet(filename)
-            valid.append(filename)
+    src = row['source']
+    url = row['url']
+    urls.append(url)
 
-    fastparquet.writer.merge([filename for filename in valid])
-    df_all = pd.read_parquet("all.pq")
-    print(df_all)
+    df2 = pd.read_csv(f'{url}')
+    if not set(df2.columns).issubset(set(cols)):
+        print(f"invalid csv: {url}")
+    else:
 
-shutil.rmtree(tmpdirname)
+        size = len(df2.index)
+
+        df2["csv"] = [url] * size
+        df2["who"] = [src] * size
+        for colname in cols:
+            if colname not in df2.columns:
+                df2[colname] = [None] * size
+
+        df2 = df2.reindex(columns=["who", "csv"] + cols)
+
+        filename = f"all.pq/csv={index}"
+        df2.to_parquet(filename)
+        valid.append(filename)
+
+fastparquet.writer.merge([filename for filename in valid])
+df_all = pd.read_parquet('all.pq')
+print(df_all)
