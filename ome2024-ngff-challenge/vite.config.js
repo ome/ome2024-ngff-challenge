e23331ee1f490caa6ee0188c13d5f9b6bdf217d1
Josh Moore <josh@openmicroscopy.org>
2024-10-08 13:08:09 +0200

    Update vite.config.js

--- ome2024-ngff-challenge/vite.config.js
+++ ome2024-ngff-challenge/vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
+let config = {
+  plugins: [svelte()],
+};
+// this will be undefined when deployed from netlify, but is used by gh-pages
+if (process.env.GITHUB_REPOSITORY_OWNER) {
+  config.base = `https://${process.env.GITHUB_REPOSITORY_OWNER}.github.io/ome2024-ngff-challenge/`;
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
