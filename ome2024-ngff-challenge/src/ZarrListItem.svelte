8ea7d0851f7d23e0d1d5072c170f788073cdc955
William Moore <w.moore@dundee.ac.uk>
2024-10-09 07:17:09 +0100

    Add validator and vizarr links, dims in table

--- ome2024-ngff-challenge/src/ZarrListItem.svelte
+++ ome2024-ngff-challenge/src/ZarrListItem.svelte
@@ -1,7 +1,7 @@
 <script>
   import { onMount } from "svelte";
   import { filesizeformat } from "./util";
-  import {loadMultiscales} from "./tableStore";
+  import { loadMultiscales } from "./tableStore";
   import Thumbnail from "./Thumbnail.svelte";
 
   export let rowData;
@@ -18,7 +18,6 @@
     imgAttrs = img[0];
     imgUrl = img[1];
     plateAttrs = img[2]; // optional
-
   });
 </script>
 
@@ -28,14 +27,50 @@
       <Thumbnail source={imgUrl} attrs={imgAttrs} max_size={2000} />
     {/if}
   </div>
-  T:{rowData.size_t} C:{rowData.size_c} Z:{rowData.size_z} X:{rowData.size_x} Y:{rowData.size_y} Size: {filesizeformat(rowData.written)}
-
+  <table style:float={listIndex % 2 == 0 ? "right" : "left"}>
+    <tr><td>T</td><td>{rowData.size_t}</td></tr>
+    <tr><td>C</td><td>{rowData.size_c}</td></tr>
+    <tr><td>Z</td><td>{rowData.size_z}</td></tr>
+    <tr><td>X</td><td>{rowData.size_x}</td></tr>
+    <tr><td>Y</td><td>{rowData.size_y}</td></tr>
+    <tr><td>Size</td><td>{filesizeformat(rowData.written)}</td></tr>
+  </table>
+  <div style:float={listIndex % 2 == 0 ? "right" : "left"}>
+    <a
+      title="Validator: {rowData.url}"
+      href="https://deploy-preview-36--ome-ngff-validator.netlify.app/?source={rowData.url}"
+      target="_blank"
+      ><img alt="OME logo" class="link_logo" src="/ome-logomark.svg" />
+    </a>
+    <a
+      title="Vizarr: {rowData.url}"
+      href="https://hms-dbmi.github.io/vizarr/?source={rowData.url}"
+      target="_blank"
+      ><img alt="Vizarr logo" class="link_logo" src="/vizarr_logo.png" />
+    </a>
+  </div>
 </div>
-
 
 <style>
   .zarr-list-item {
     padding: 10px;
-    color: royalblue;
+    color: lightgray;
   }
-  </style>
+  table {
+    float: left;
+    margin-left: 10px;
+  }
+  td {
+    padding: 1px;
+    font-size: 80%;
+    line-height: normal;
+  }
+  .link_logo {
+    width: 24px;
+    height: 24px;
+    visibility: hidden;
+  }
+  .zarr-list-item:hover .link_logo {
+    visibility: visible;
+  }
+</style>
